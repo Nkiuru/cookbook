65336a00fa04bec495dbe9f5447add1688e6cdb1
Niklas Kiuru <niklas.kiuru1@gmail.com>
2020-03-31 20:47:26 +0300

    Closes #9, Fix authorization headers parsing & add missing export

--- server/src/modules/auth/resolvers/me.js
+++ server/src/modules/auth/resolvers/me.js
@@ -2,3 +2,5 @@ const me = async (_, args, { user }) => ({
   ...user._doc,
   id: user.id,
 });
+
+module.exports = me;
--- server/src/utils/context.js
+++ server/src/utils/context.js
@@ -1,21 +1,22 @@
 const tokenUtil = require('./token.js');
 const User = require('../models/user');
 
-const TOKEN_HEADER_NAME = 'x-token';
+const TOKEN_HEADER_NAME = 'authorization';
 
 const getUser = async req => {
   if (!req) {
     return null;
   }
   const tokenHeader = req.get(TOKEN_HEADER_NAME);
-
-  if (!tokenHeader) {
+  const token = tokenHeader.split(' ')[1];
+  if (!token) {
     return null;
   }
 
   try {
-    const decodedToken = await tokenUtil.getDecodedToken(tokenHeader);
-    return await User.findById(decodedToken.userId);
+    const decodedToken = await tokenUtil.getDecodedToken(token);
+    const user = await User.findById(decodedToken.userId);
+    return user;
   } catch (e) {
     return null;
   }
