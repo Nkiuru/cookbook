28b1c3110a0029f5c4de81bcdde4611e52ba6b43
Niklas Kiuru <niklas.kiuru1@gmail.com>
2020-05-02 10:11:24 +0300

    Setup react-router

--- front-end/src/app/App.js
+++ front-end/src/app/App.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import { Route, Switch, useLocation } from 'react-router';
 import './App.scss';
 import ApolloClient from 'apollo-boost';
 import { ApolloProvider } from '@apollo/react-hooks';
 import HomePage from '../pages/HomePage';
+import { authenticated as auth } from '../utils/auth';
+
 const API_URL = 'http://localhost:3000/graphql';
 
 const client = new ApolloClient({
@@ -18,12 +21,21 @@ const client = new ApolloClient({
 });
 
 function App() {
+  const location = useLocation();
+  const home = () => <HomePage />;
+  //const dashboard =() =>  (auth() ? <Redirect to="/" /> : <DashboardPage />);
+  //const profile = () => (auth() ? <Redirect to="/" /> : <ProfilePage />);
+  //const recipes =() =>  (auth() ? <Redirect to="/" /> : <RecipesPage />);
+  //const recipeDetail = () => (auth() ? <Redirect to="/" /> : <RecipeDetailPage />);
+  //const recipeEdit = () => (auth() ? <Redirect to="/" /> : <AddEditRecipePage />);
+  //const recipeSearch = () => (auth() ? <Redirect to="/" /> : <RecipeSearchPage />);
+  //const listSearch = () => (auth() ? <Redirect to="/" /> : <ListSearchPage />);
+  //const settings = () => (auth() ? <Redirect to="/" /> : <SettingsPage />);
   return (
     <ApolloProvider client={client}>
-      <div>
-        <h2>My first Apollo app</h2>
-        <HomePage />
-      </div>
+      <Switch location={location}>
+        <Route path="/" render={home} />
+      </Switch>
     </ApolloProvider>
   );
 }
