973cbc35d82ee01ce199a2939b16d2217bcfc5a0
Niklas Kiuru <niklas.kiuru1@gmail.com>
2020-05-05 18:49:49 +0300

    Fix cors

--- server/src/app.js
+++ server/src/app.js
@@ -32,11 +32,7 @@ app.use((req, res, next) => {
   }
 });
 
-const corsOptions = {
-  origin: ['http://localhost:3001', 'https://env-6758651.jelastic.metropolia.fi'],
-  credentials: true,
-};
-app.use(cors(corsOptions));
+app.use(cors());
 
 server.applyMiddleware({
   path: '/graphql',
